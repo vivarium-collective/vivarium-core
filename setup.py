1ccd691233dc9db903dfe7e0ce336f8c78b3d051
cs <cs.temporary@icloud.com>
2020-09-17 19:27:16 +0000

    Bump version to 0.0.20

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", 'r') as readme:
 
 setup(
     name='vivarium-core',
-    version='0.0.19',
+    version='0.0.20',
     packages=[
         'vivarium',
         'vivarium.core',
