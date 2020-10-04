55c712169e6af610f9af2d5b7176d0851a5f54e4
Eran Agmon <agmon.eran@gmail.com>
2020-10-03 22:19:20 -0700

    meta_division test

--- vivarium/processes/meta_division.py
+++ vivarium/processes/meta_division.py
@@ -1,9 +1,25 @@
 from __future__ import absolute_import, division, print_function
 
+import os
 import uuid
 import logging as log
 
-from vivarium.core.process import Deriver
+from vivarium.core.process import (
+    Deriver,
+    Generator
+)
+from vivarium.core.composition import (
+    compartment_hierarchy_experiment,
+    PROCESS_OUT_DIR,
+)
+from vivarium.core.experiment import pp
+
+# processes
+from vivarium.processes.exchange_a import ExchangeA
+from vivarium.processes.timeline import TimelineProcess
+
+
+NAME = 'meta_division'
 
 
 # functions for generating daughter ids
@@ -24,7 +40,7 @@ def divider_set_false(state):
 
 class MetaDivision(Deriver):
 
-    name = 'meta_division'
+    name = NAME
     defaults = {
         'initial_state': {},
         'daughter_path': ('agents',),
@@ -86,3 +102,115 @@ class MetaDivision(Deriver):
                         'daughters': daughter_updates}}}
         else:
              return {}   
+
+
+
+# test
+class ToyAgent(Generator):
+    defaults = {
+        'exchange': {'uptake_rate': 0.1},
+        'daughter_path': tuple(),
+        'agents_path': ('..', '..', 'agents')}
+
+    def generate_processes(self, config):
+        daughter_path = config['daughter_path']
+        agent_id = config['agent_id']
+        division_config = dict(
+            {},
+            daughter_path=daughter_path,
+            agent_id=agent_id,
+            compartment=self)
+
+        return {
+            'exchange': ExchangeA(config['exchange']),
+            'division': MetaDivision(division_config)}
+
+    def generate_topology(self, config):
+        agents_path = config['agents_path']
+        return {
+            'exchange': {
+                'internal': ('internal',),
+                'external': ('external',)},
+            'division': {
+                'global': ('global',),
+                'agents': agents_path,
+            }
+        }
+
+
+def test_division():
+    agent_id = '1'
+
+    # initial state
+    initial_state = {
+        'agents': {
+            agent_id: {
+                'internal': {'A': 0},
+                'external': {'A': 1},
+                'global': {
+                    'divide': False
+                }
+            },
+        }
+    }
+
+    # timeline triggers division
+    time_divide = 5
+    time_total = 10
+    timeline = [
+        (0, {('agents', agent_id, 'global', 'divide'): False}),
+        (time_divide, {('agents', agent_id, 'global', 'divide'): True}),
+        (time_total, {})]
+
+    # declare the hierarchy
+    hierarchy = {
+        'processes': [
+            {
+                'type': TimelineProcess,
+                'config': {'timeline': timeline},
+                'topology': {
+                    'global': ('global',),
+                    'agents': ('agents',)
+                }
+            }
+        ],
+        'agents': {
+            'generators': [
+                {
+                    'name': agent_id,
+                    'type': ToyAgent,
+                    'config': {'agent_id': agent_id}
+                },
+            ]
+        }
+    }
+
+    # configure experiment
+    experiment = compartment_hierarchy_experiment(
+        hierarchy=hierarchy,
+        initial_state=initial_state)
+
+    # run simulation
+    experiment.update(time_total)
+    output = experiment.emitter.get_data()
+    experiment.end()
+
+    # external starts at 1, goes down until death, and then back up
+    # internal does the inverse
+    assert list(output[time_divide]['agents'].keys()) == [agent_id]
+    assert agent_id not in list(output[time_divide + 1]['agents'].keys())
+    assert len(output[time_divide]['agents']) == 1
+    assert len(output[time_divide + 1]['agents']) == 2
+
+    return output
+
+def run_division():
+    out_dir = os.path.join(PROCESS_OUT_DIR, NAME)
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    output = test_division()
+    pp(output)
+
+
+if __name__ == '__main__':
+    run_division()
