3e364606000127606aaa05c4d77e70b6294a5867
Eran Agmon <agmon.eran@gmail.com>
2020-10-06 08:49:08 -0700

    fix docstring

--- vivarium/core/experiment.py
+++ vivarium/core/experiment.py
@@ -1574,16 +1574,17 @@ def print_progress_bar(
         fill='█',
         print_end="\r"
 ):
-    """
-    Call in a loop to create terminal progress bar
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-        print_end    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """ Call in a loop to create terminal progress bar
+
+    Arguments:
+        iteration: (Required) current iteration
+        total:     (Required) total iterations
+        prefix:    (Optional) prefix string
+        suffix:    (Optional) suffix string
+        decimals:  (Optional) positive number of decimals in percent complete
+        length:    (Optional) character length of bar
+        fill:      (Optional) bar fill character
+        print_end: (Optional) end character
     """
     progress = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filled_length = int(length * iteration // total)
