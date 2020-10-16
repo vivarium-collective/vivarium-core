fd6128e6728ef2bcdbe7d51592f192af764eaa11
Eran Agmon <agmon.eran@gmail.com>
2020-10-16 08:59:54 -0700

    v0.0.35

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", 'r') as readme:
 
 setup(
     name='vivarium-core',
-    version='0.0.34',
+    version='0.0.35',
     packages=[
         'vivarium',
         'vivarium.core',
