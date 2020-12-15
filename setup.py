3a148f54b745e210346cfdbcc88a32905c7d51de
Ryan Spangler <ryan.spangler@gmail.com>
2020-12-14 16:40:45 -0800

    breaking api change, major version shift

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", 'r') as readme:
 
 setup(
     name='vivarium-core',
-    version='0.0.38',
+    version='0.1.0',
     packages=[
         'vivarium',
         'vivarium.core',
