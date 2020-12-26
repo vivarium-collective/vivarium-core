8065b5961183624461c8bfd939959ea2ba18817b
Eran Agmon <agmon.eran@gmail.com>
2020-12-26 10:43:02 -0800

    add test for topology plot

--- vivarium/plots/topology.py
+++ vivarium/plots/topology.py
@@ -1,21 +1,21 @@
 import os
 
 import numpy as np
-
 import matplotlib.pyplot as plt
 import networkx as nx
 
+from vivarium.core.process import Process, Generator
+from vivarium.core.experiment import Experiment
 
-def get_network(
-    generator,
-    settings={},
+
+
+def get_graph(
+        bigraph,
+        settings={}
 ):
-    network = generator.generate({})
-    topology = network['topology']
-    processes = network['processes']
+    topology = bigraph['topology']
+    processes = bigraph['processes']
 
-    # make graph from topology
-    G = nx.Graph()
     process_nodes = []
     store_nodes = []
     edges = {}
@@ -23,7 +23,6 @@ def get_network(
         process_id = str(process_id)
         process_id = process_id.replace("'", "").replace("_", "_\n")
         process_nodes.append(process_id)
-        G.add_node(process_id)
 
         for port, store_id in connections.items():
             store_id = str(store_id)
@@ -31,19 +30,34 @@ def get_network(
 
             if store_id not in store_nodes:
                 store_nodes.append(store_id)
-            if store_id not in list(G.nodes):
-                G.add_node(store_id)
 
             edge = (process_id, store_id)
             edges[edge] = port
-            G.add_edge(process_id, store_id)
 
     # are there overlapping names?
     overlap = [name for name in process_nodes if name in store_nodes]
     if overlap:
         print('{} shared by processes and stores'.format(overlap))
 
-    return G, process_nodes, store_nodes
+    return process_nodes, store_nodes, edges
+
+
+def get_networkx_graph(
+    bigraph,
+    settings={},
+):
+    process_nodes, store_nodes, edges = get_graph(bigraph)
+
+    # make networkX graph
+    G = nx.Graph()
+    for node_id in process_nodes:
+        G.add_node(node_id)
+    for node_id in store_nodes:
+        G.add_node(node_id)
+    for (process_id, store_id), port in edges.items():
+        G.add_edge(process_id, store_id)
+
+    return G, process_nodes, store_nodes, edges
 
 
 
@@ -68,11 +82,10 @@ def plot_topology(
     # get figure settings
     show_ports = settings.get('show_ports', True)
 
-    G, process_nodes, store_nodes = get_network(
-        generator,
-        settings,
-    )
-
+    bigraph = generator.generate({})
+    G, process_nodes, store_nodes, edges = get_networkx_graph(
+        bigraph,
+        settings)
 
     # get positions
     pos = {}
@@ -135,3 +148,79 @@ def plot_compartment_topology(
 ):
     return plot_topology(
         compartment, settings, out_dir, filename)
+
+
+
+def test_topology():
+    class MultiPort(Process):
+        name = 'multi_port'
+        def ports_schema(self):
+            return {
+                'A': {
+                    'a': {
+                        '_default': 0,
+                        '_emit': True}},
+                'B': {
+                    'a': {
+                        '_default': 0,
+                        '_emit': True}},
+                'C': {
+                    'a': {
+                        '_default': 0,
+                        '_emit': True}}}
+
+        def next_update(self, timestep, states):
+            return {
+                'A': {'a': 1},
+                'B': {'a': 1},
+                'C': {'a': 1}}
+
+
+    class MergePort(Generator):
+        """combines both of MultiPort's ports into one store"""
+        name = 'multi_port_generator'
+
+        def generate_processes(self, config):
+            return {
+                'multi_port1': MultiPort({}),
+                'multi_port2': MultiPort({}),
+            }
+
+        def generate_topology(self, config):
+            return {
+                'multi_port1': {
+                    'A': ('aaa',),
+                    'B': ('bbb',),
+                    'C': ('aaa',),
+                },
+                'multi_port2': {
+                    'A': ('aaa',),
+                    'B': ('aaa',),
+                    'C': ('ccc',),
+                },
+            }
+
+
+    merge_port = MergePort({})
+    # network = merge_port.generate()
+    # exp = Experiment({
+    #     'processes': network['processes'],
+    #     'topology': network['topology']})
+
+    plot_topology(
+        merge_port,
+        settings={},
+        out_dir='out',
+        filename='merge_port_topology',
+    )
+
+    # bigraph = merge_port.generate()
+    # process_nodes, store_nodes, edges = get_graph(bigraph)
+
+
+
+    import ipdb; ipdb.set_trace()
+
+
+if __name__ == '__main__':
+    test_topology()
