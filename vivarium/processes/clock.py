23b811465585f86fc2770a831bf7b389409daa74
Eran Agmon <agmon.eran@gmail.com>
2021-02-04 11:22:46 -0800

    clock initial state

--- vivarium/processes/clock.py
+++ vivarium/processes/clock.py
@@ -14,11 +14,14 @@ class Clock(Process):
     defaults = {
         'time_step': 1.0}
 
+    def initial_state(self, config=None):
+        return {}
+
     def ports_schema(self):
         return {
             'global_time': {
                 '_default': 0.0,
                 '_updater': 'accumulate'}}
-
+    
     def next_update(self, timestep, states):
         return {'global_time': timestep}
