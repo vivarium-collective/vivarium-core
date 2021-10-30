eee49a45962d8e18ee5fb92376cccc4114c9266b
cs <cs.temporary@icloud.com>
2021-10-30 23:10:20 +0000

    Fix process.schema type

--- vivarium/core/process.py
+++ vivarium/core/process.py
@@ -115,7 +115,7 @@ class Process(metaclass=abc.ABCMeta):
                 '_updater': 'set'}))
 
         self.parameters.setdefault('time_step', DEFAULT_TIME_STEP)
-        self.schema = None
+        self.schema: Optional[dict] = None
 
     def initial_state(self, config: Optional[dict] = None) -> State:
         """Get initial state in embedded path dictionary.
