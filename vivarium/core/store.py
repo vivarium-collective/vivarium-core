bf0bf6e6ed08b380447cf506027f57c054c281de
Eran <agmon.eran@gmail.com>
2023-05-16 09:19:02 -0400

    import registries from registry module to fix bug

--- vivarium/core/store.py
+++ vivarium/core/store.py
@@ -16,7 +16,7 @@ import numpy as np
 from pint import Unit, Quantity
 from typing import Optional
 
-from vivarium import divider_registry, serializer_registry, updater_registry
+from vivarium.core.registry import divider_registry, serializer_registry, updater_registry
 from vivarium.core.process import ParallelProcess, Process
 from vivarium.library.dict_utils import deep_merge, deep_merge_check, MULTI_UPDATE_KEY
 from vivarium.library.topology import dict_to_paths
