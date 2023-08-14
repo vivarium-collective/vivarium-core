781fa13c05944a13c5fb8e89298c538715a0e346
Eran <agmon.eran@gmail.com>
2023-08-14 11:38:55 -0400

    add update argument description to docstring

--- vivarium/core/store.py
+++ vivarium/core/store.py
@@ -1472,6 +1472,7 @@ class Store:
 
             * source - the source path from an outer process port
             * target - the location where the node will be placed.
+            * update (optional) - an update to the node being moved.
 
         * `_generate` - The value has four keys, which are essentially
           the arguments to the `generate()` function:
